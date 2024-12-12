9b60aee10a8be68dbfec158de540fbe6b8406a93
Jason Zhu <jason.zhu@rock-chips.com>
2024-12-12 11:13:49 +0800

    qemu: riscv64: support ext4
    Signed-off-by: Jason Zhu <jason.zhu@rock-chips.com>
    Change-Id: I555852f3c71a60bf9ed59eed8d206b523bb4aaec

--- qemu/run-riscv64.sh
+++ qemu/run-riscv64.sh
@@ -1,1 +1,1 @@
-qemu-system-riscv64 -M virt -m 4096M -nographic -bios ../opensbi/build/platform/generic/firmware/fw_jump.bin -kernel ../u-boot/u-boot-nodtb.bin -drive file=../buildroot/output/images/rootfs.ext2,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -append "root=/dev/vda rw console=ttyS0"
+qemu-system-riscv64 -M virt -m 4096M -nographic -bios ../opensbi/build/platform/generic/firmware/fw_jump.bin -kernel ../u-boot/u-boot-nodtb.bin -drive file=../buildroot/output/images/rootfs.ext4,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -append "root=/dev/vda rw console=ttyS0"
